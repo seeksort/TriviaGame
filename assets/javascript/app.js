4fe2e12430abae86dc9053fef915280f78ab43d7
Kristin Faner <kfaner@gmail.com>
2016-08-21 15:29:15 -0500

    styles, media queries, initial object for questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+var questionBank = {
+	q1: 
+	{
+		question: "This country is made up of approximately 7,600 islands.",
+		answerChoices: ["The Philippines", "Indonesia", "Singapore", "Malaysia"],
+		answerInd: 0,
+		picture: "#"
+	},
+	q2:
+	{ 
+		question: "This is the highest mountain in the Southern Hemisphere.",
+		answerChoices: ["K2", "Aconcagua", "Vinson Massif", "Mauna Kea"],
+		answerInd: 1,
+		picture: "#"
+	},
+	q3:
+	{
+		question: "The Amazon River flows through each of these countries EXCEPT:",
+		answerChoices: ["Columbia", "Brazil", "Ecuador", "Venezuela"],
+		answerInd: 3,
+		picture: "#"
+	},
+	q4:
+	{ 
+		question: "In India, severe storm systems are called:",
+		answerChoices: ["Hurricanes", "Cyclones", "Typhoons", "Super Storms"],
+		answerInd: 1,
+		picture: "#"
+	},
+	q5:
+	{
+		question: "Which of these is the driest hot desert?",
+		answerChoices: ["Gobi Desert", "Sahara Desert", "Atacama Desert", "Gibson Desert"],
+		answerInd: 2,
+		picture: "#"
+	},
+	q6:
+	{
+		question: "What is a ring-shaped coral reef called?",
+		answerChoices: ["Fringing Reef", "Barrier Reef", "Cay", "Atoll"],
+		answerInd: 3,
+		picture: "#"
+	},
+	q7:
+	{
+		question: "Which of these animals is NOT found in Antarctica?",
+		answerChoices: ["Adelie Penguin", "Weddell Seal", "Gentoo Penguin", "Black-tailed Gull"],
+		answerInd: 3,
+		picture: "#"
+	},
+	q8:
+	{
+		question: "What is the world's largest island?",
+		answerChoices: ["Greenland", "Australia", "Madagascar", "Indonesia"],
+		answerInd: 0,
+		picture: "#"
+	}
+}
